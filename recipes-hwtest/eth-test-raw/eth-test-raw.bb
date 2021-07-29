b03d059346755ae4ee67d39a0c08e74962531ca4
KlausPopp <klauspopp@gmx.de>
2021-07-29 16:47:13 +0200

    add netio_monitor skeleton

--- recipes-hwtest/eth-test-raw/eth-test-raw.bb
+++ recipes-hwtest/eth-test-raw/eth-test-raw.bb
@@ -1,9 +1,9 @@
 DESCRIPTION = "Raw Ethernet Test Client"
 
-LICENSE = "Apache-2.0"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
-SRC_URI = "git://github.com/ci4rail/eth-test-raw/;protocol=https;branch=initial"
+SRC_URI = "git://github.com/ci4rail/eth-test-raw/;protocol=https;branch=main"
 
 SRCREV = "${AUTOREV}"
 
---
+++ recipes-hwtest/netio_monitor/files/netio_monitor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import os
+import sys
+from time import perf_counter as time
+
+
+def write_test(file, block_size, blocks_count):
+    f = os.open(file, os.O_CREAT | os.O_WRONLY, 0o777)  # low-level I/O
+
+    start = time()
+    for i in range(blocks_count):
+        buff = bytearray(block_size)
+        buff[0] = i & 0xFF
+        os.write(f, buff)
+        os.fsync(f)  # force write to disk
+
+    os.close(f)
+    return time() - start
+
+
+def read_test(file, block_size, blocks_count):
+    f = os.open(file, os.O_RDONLY)  # low-level I/O
+
+    start = time()
+    for i in range(blocks_count):
+        buff = os.read(f, block_size)
+        if buff[0] != i & 0xFF:
+            raise RuntimeError(f"verify error at block {i}, data read: {buff[0]}")
+
+    os.close(f)
+    return time() - start
+
+
+def main(file, block_size, blocks_count):
+    print(f"write: {write_test(file, block_size, blocks_count)}")
+    print(f"read: {write_test(file, block_size, blocks_count)}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
